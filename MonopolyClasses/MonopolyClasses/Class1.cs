26be475c271754fc47c7df124705738e49e2b310
Andrey <bondarenko.andrey13@gmail.com>
2016-10-11 20:27:48 +0300

    first test commit

---
+++ MonopolyClasses/MonopolyClasses/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MonopolyClasses
+{
+    class Player { }
+    class GameCell { }
+
+    class Game
+    {
+        public Player Player { get; set; }
+        public GameCell[] GameCells { get; }
+        private Timer waitPlayerConnectionTimer;
+        private Timer waitPlayerMoveTimer;
+        //public int GameIteration { get; set; } // ???
+    }
+}
